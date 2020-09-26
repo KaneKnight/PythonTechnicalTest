5d922a5a00f77262f22878a7ae093a88c77787d1
Kane Knight <kane.knight@hotmail.com>
2020-09-26 20:36:26 +0100

    Create super user and define bond.

--- origin/bonds/models.py
+++ origin/bonds/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+class Bond(models.Model):
+    isin = models.CharField(max_length=30)
+    size = models.IntegerField()
+    currency = models.CharField(max_length=6)
+    maturity = models.DateField()
+    lei = models.CharField(max_length=30)
+    legal_name = models.CharField(max_length=30)
