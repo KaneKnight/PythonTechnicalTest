3e4f68eabff5532733add080becfe8ed62fc97a0
Kane Knight <kane.knight@hotmail.com>
2020-09-27 01:56:08 +0100

    Create bond test.

--- origin/bonds/tests.py
+++ origin/bonds/tests.py
@@ -1,7 +1,11 @@
-from rest_framework.test import APISimpleTestCase
+from django.test import TestCase
+from .models import Bond
 
+class BondTestCase(TestCase):
+    def setUp(self):
+        Bond.objects.create(isin="X10146", size=10, currency="GBP", maturity="2000-01-01", lei="1234", legal_name="Fake", userid=1)
 
-class HelloWorld(APISimpleTestCase):
-    def test_root(self):
-        resp = self.client.get("/")
-        assert resp.status_code == 200
+    def test_bond_creation(self):
+        """Animals that can speak are correctly identified"""
+        fake = Bond.objects.get(isin="X10146")
+        self.assertEqual(fake.lei, "1234")
